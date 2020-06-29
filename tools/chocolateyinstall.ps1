39d3a4cfee950b843a917e5224a88135d712f1f6
Mike Parks <mparks@tkware.info>
2020-06-29 09:38:35 -0600

    Remove extra url var and extra spaces

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,15 +6,14 @@ $url        = 'https://tetr.io/about/desktop/builds/TETR.IO%20Setup.exe'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' 
+  fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
 
-  softwareName  = 'TETR.IO*' 
+  softwareName  = 'TETR.IO*'
 
   checksum      = '21C48774B03751E5401F8F9FB391D03BB70F208E09024977266F12FC73DAE1E7'
-  checksumType  = 'sha256' 
+  checksumType  = 'sha256'
   silentArgs   = '/S' # NSIS
 }
 
-Install-ChocolateyPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
